9dd5f9744c7f77635789f262e90dfd38091b14af
Henok <henokmicrometre@gmail.com>
2023-12-15 16:56:11 +0000

    sorted alprd

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,33 +1,10 @@
 import React from "react"
+import Alprd from "./components/alprd"
 
-function App() {
-  const [state, setState] = React.useState([]);
-  React.useEffect(() => {
-    const evtSource = new EventSource("http://localhost:5000/alprdsse");
-    evtSource.onmessage = (event) => {
-      const myEvent = JSON.parse(event.data);
-      const alprdData = JSON.parse(event.data)
-      const alprdDataUuid = alprdData.uuid;
-      const alprdDataResult = alprdData.results[0];
-      const alprdDataPlate = alprdData.results[0].plate;
-      setState(alprdDataResult);
-    };
-    evtSource.onopen = (event) => {
-      console.log(event);
-    };
-  }, []);
-  return (
+export default function App(){
+  return(
     <>
-      <div>
-        {Object.entries(state).map(([id, { photoURL, email }]) => (
-          <div>
-            <h3>{id}</h3>
-          </div>
-        ))}
-      </div>
-
+    <Alprd/>
     </>
-  );
+  )
 }
-
-export default App;
--- react-client/src/components/alprd.js
+++ react-client/src/components/alprd.js
@@ -1,7 +1,30 @@
-export default function Profile() {
+import React from "react"
+export default function Alprd() {
+  const [state, setState] = React.useState([]);
+  const [stateUuid, setUud] = React.useState([]);
+  React.useEffect(() => {
+    const evtSource = new EventSource("http://localhost:5000/alprdsse");
+    evtSource.onmessage = (event) => {
+      const alprdData = JSON.parse(event.data)
+      const alprdDataUuid = alprdData.uuid;
+      const alprdDataResultArray = alprdData.results[0];
+      const alprdDataResultArrayMap = Object.keys(alprdDataResultArray).map((key) => [key, alprdDataResultArray[key]]);
+      setUud(alprdDataUuid)
+      console.log(alprdDataUuid)
+      setState(alprdDataResultArrayMap);
+    };
+    evtSource.onopen = (event) => {
+      console.log(event);
+    };
+  }, []);
   return (
     <>
-    <h1>test</h1>
+<h1>
+  {stateUuid}
+  {state[4]}
+  {state[1]}
+  {state[7]}
+</h1>
     </>
-  )
+  );
 }
