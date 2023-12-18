34a3effe06cba582ed64d558e43d71c5306d4fe5
Henok <henokmicrometre@gmail.com>
2023-12-18 11:29:11 +0000

    added side

---
+++ nextjs-client/app/dashboard/layout.js
@@ -0,0 +1,20 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+
+
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      </div>
+      {children}
+    </section>
+  )
+}
