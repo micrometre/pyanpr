ffc8c2467f5306d476ead76d2180bb24318e93a4
Henok <henokmicrometre@gmail.com>
2023-12-25 14:23:03 +0000

    sorted upload

--- react-client/src/components/sidenav.js
+++ react-client/src/components/sidenav.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { PowerIcon } from '@heroicons/react/24/outline';
 import AcmeLogo from './acme-logo';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+  VideoCameraIcon,
 
+} from '@heroicons/react/24/outline';
 
 export default function SideNav() {
   return (
@@ -15,7 +21,9 @@ export default function SideNav() {
         </div>
       </a>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <Uploader />
+            <FileUpload />
+      </div>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <form>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
@@ -27,19 +35,52 @@ export default function SideNav() {
   );
 }
 
-export function Uploader() {
+
+
+export function FileUpload(props) {
+  const [image, setImage] = React.useState(null);
+  const [createObjectURL, setCreateObjectURL] = React.useState(null);
+
+  const uploadToClient = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+
+      setImage(i);
+      setCreateObjectURL(URL.createObjectURL(i));
+    }
+  };
+
+  const uploadToServer = async (event) => {
+    const body = new FormData();
+    body.append("file", image);
+    const response = await fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body
+    });
+  };
+
   return (
-    <>
-      <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block">
-        <form>
-          <h1>React File Upload</h1>
-          <input type="file" />
-          <button type="submit">Upload</button>
-        </form>
-      </div>
-    </>
-  )
+    <div>
+      <p
+        className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
 
+      >
+        <input type="file" name="myImage" onChange={uploadToClient} />
+      </p>
+      <button
+        type="submit"
+        onClick={uploadToServer}
+        className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+      >
+        <VideoCameraIcon/>
 
+        <p
 
+        >
+
+          Send Video file to server
+        </p>
+      </button>
+    </div>
+  );
 }
