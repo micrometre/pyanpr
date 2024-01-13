671856b3c759723226342193d598f23bbbbdd2ca
Henok <henokmicrometre@gmail.com>
2024-01-13 07:52:30 +0000

    exec request

--- react-client/src/components/upload-db.js
+++ react-client/src/components/upload-db.js
@@ -1,24 +1,45 @@
 import React from "react"
+import { useInterval } from "./useInterval";
 
 
-const url = "http://172.187.216.226:5000/upload"
-export default function DataTable() {
-  const [items, setItems] = React.useState([]);
-  React.useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setItems(data));
-  }, []);
-  console.log(items);
-  const collection = items.map((plate, index) => {
+
+export default function UploadDb() {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  useInterval(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const response = await fetch('http://172.187.216.226:5000/upload');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, 5000);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  const  revdata = data.reverse();
+  console.log(revdata)
+  const collection = data.map((plate, index) => {
     return (
         <li  key={index}>{plate}</li>
     );
   });
   return (
-    <
-    >
+    <div>
       {collection} {/* Render the collection of items */}
-    </>
+    </div>
   );
 }
