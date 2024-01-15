d1a07ff536e3d86bda969a0a15a94f5982d7f795
Henok <henokmicrometre@gmail.com>
2024-01-15 18:49:33 +0000

    added flask

--- static/js/main.js
+++ static/js/main.js
@@ -27,3 +27,20 @@ if (typeof (EventSource) !== "undefined") {
   } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
   }
+
+
+
+
+      function handleFileSelect(evt) {
+        for (const file of evt.target.files) {
+          const div = document.createElement('div')
+          const src = URL.createObjectURL(file)
+          div.innerHTML =
+            `<img style="height: 250px; border: 1px solid #000; margin: 5px"` +
+            `src="${src}" title="${escape(file.name)}">`
+
+          document.getElementById('list').insertBefore(div, null)
+        }
+      }
+
+      document.getElementById('files').addEventListener('change', handleFileSelect, false);
