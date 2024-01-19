223fe92fdf4d7a1003596589509588d114f7cc09
Henok <henokmicrometre@gmail.com>
2024-01-19 19:58:43 +0000

    adding bootstrap

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,13 +1,29 @@
 function handleFileSelect(evt) {
-    for (const file of evt.target.files) {
-      const div = document.createElement('div')
-      const src = URL.createObjectURL(file)
-      div.innerHTML =
-        `<img style="height: 250px; border: 1px solid #000; margin: 5px"` +
-        `src="${src}" title="${escape(file.name)}">`
-  
-      document.getElementById('list').insertBefore(div, null)
+  for (const file of evt.target.files) {
+    const div = document.createElement('div')
+    const src = URL.createObjectURL(file)
+    div.innerHTML =
+      `<img style="height: 250px; border: 1px solid #000; margin: 5px"` +
+      `src="${src}" title="${escape(file.name)}">`
+
+    document.getElementById('list').insertBefore(div, null)
+  }
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+
+function update() {
+  var element = document.getElementById("videoprogressBar");
+  var width = 1;
+  var identity = setInterval(scene, 10);
+  function scene() {
+    if (width >= 100) {
+      clearInterval(identity);
+    } else {
+      width++;
+      element.style.width = width + '%';
     }
   }
-  
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+}
