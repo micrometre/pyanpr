6164a7390ca1e9a5e545aef501a79bb091572b12
Henok <henokmicrometre@gmail.com>
2024-01-26 20:57:06 +0000

    adde react

--- static/js/react.js
+++ static/js/react.js
@@ -1,3 +1,17 @@
+const { useState } = React
+
+const UploadDb = (props) => { 
+    const [text, setText] = useState('hello');
+
+    return (
+      <div>
+        <h1>{text}</h1>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+    );
+  }
+
+
 function ReactApp() {
     return (
         <div>React App </div>
@@ -5,10 +19,13 @@ function ReactApp() {
 }
 
 
+
+
 function ReactAppFromCDN() {
     return (
         <div>
 
+            <UploadDb></UploadDb>
             <ReactApp></ReactApp>
             with CDN
 
