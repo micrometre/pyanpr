8a91d09aa68cf2645449d3737709be0764e2b165
Henok <henokmicrometre@gmail.com>
2024-03-04 04:19:25 +0000

    added gui

---
+++ scripts/mng.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo rm -rf  /home/dell/repos/pyanpr/static/tmp/
+sudo rm -rf /home/dell/repos/pyanpr/static/images/
+sudo rm -rf /home/dell/repos/pyanpr/static/camera-images/
+mkdir /home/dell/repos/pyanpr/static/camera-images/
+mkdir /home/dell/repos/pyanpr/static/images
+mkdir /home/dell/repos/pyanpr/static/tmp
